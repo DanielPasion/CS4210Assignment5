3a2679051d9a488c287cb157df0b0ba96e2736e1
Daniel Pasion <dnpaxion@gmail.com>
2023-05-13 04:08:38 -0700

    Completed clustering Python file

--- clustering.py
+++ clustering.py
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Daniel Pasion
+# FILENAME: clustering.py
+# SPECIFICATION: to do clustering algorithm assignments
 # FOR: CS 4210- Assignment #5
 # TIME SPENT: how long it took you to complete the assignment
 #-----------------------------------------------------------*/
@@ -18,26 +18,38 @@ from sklearn import metrics
 df = pd.read_csv('training_data.csv', sep=',', header=None) #reading the data by using Pandas library
 
 #assign your training data to X_training feature matrix
-
+X_training = np.array(df.values)
+scoreArray = []
+xvalues = []
 #run kmeans testing different k values from 2 until 20 clusters
      #Use:  kmeans = KMeans(n_clusters=k, random_state=0)
      #      kmeans.fit(X_training)
      #--> add your Python code
+for k in range(2,20):
+     kmeans = KMeans(n_clusters=k, random_state=0)
+     kmeans.fit(X_training)
 
 
      #for each k, calculate the silhouette_coefficient by using: silhouette_score(X_training, kmeans.labels_)
      #find which k maximizes the silhouette_coefficient
      #--> add your Python code here
+     score = silhouette_score(X_training, kmeans.labels_)
+     scoreArray.append(score)
 
 #plot the value of the silhouette_coefficient for each k value of kmeans so that we can see the best k
 #--> add your Python code here
+for i in range(len(scoreArray)):
+     xvalues.append(i)
+plt.plot(xvalues,scoreArray)
+plt.show()
 
 #reading the test data (clusters) by using Pandas library
 #--> add your Python code here
-
+df = pd.read_csv('testing_data.csv', sep=',', header=None)
 #assign your data labels to vector labels (you might need to reshape the row vector to a column vector)
-# do this: np.array(df.values).reshape(1,<number of samples>)[0]
+# do this: 
 #--> add your Python code here
+labels = np.array(df.values).reshape(1,3823)[0]
 
 #Calculate and print the Homogeneity of this kmeans clustering
 print("K-Means Homogeneity Score = " + metrics.homogeneity_score(labels, kmeans.labels_).__str__())
